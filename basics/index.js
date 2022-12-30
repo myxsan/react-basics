e47fede8952aee654a6d458eb2d6d1707c40d2ac
myxsan <yksl3461@gmail.com>
2022-12-31 00:51:43 +0300

    ES6 module system and npm basics

---
+++ basics/index.js
@@ -0,0 +1,4 @@
+import myModule, {sub, sum } from './my-module.js'
+console.log(myModule("Musa"));
+console.log(sum(2, 3));
+console.log(sub(5, 2));
---
+++ basics/my-module.js
@@ -0,0 +1,4 @@
+export const sum = (a, b) => a + b;
+export const sub = (a, b) => a - b;
+const message = (text) => `heyy ${text}` 
+export default message
